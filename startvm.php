3b87bb345e88bc2f5d669874f871361367de3e3a
Mohan <mohanraz@gmail.com>
2016-09-16 14:39:57 +0530

    first shell script

---
+++ startvm.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<table>
+<?php 
+    $cmd="/var/www/html/createvm.sh";
+    foreach ($_POST as $key => $value) {
+       $cmd = $cmd . " " . $value; 
+    }
+    echo $cmd; 
+    exec($cmd, $fullList, $status);
+if ($status !== 0)
+{
+    exit('command didn\'t finish as expected: '.$status);
+}
+print_r($fullList);
+?>
+</table>
+</body>
+</html> 
